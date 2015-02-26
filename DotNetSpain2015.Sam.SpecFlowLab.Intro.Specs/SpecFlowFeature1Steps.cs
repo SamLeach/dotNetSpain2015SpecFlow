d5327b8682f4b529d202b9c37850cb0e52d8ba42
Sam Leach <leachsam@gmail.com>
2015-02-26 17:18:04 +0100

    Simple Calculator Example

---
+++ DotNetSpain2015.Sam.SpecFlowLab.Intro.Specs/SpecFlowFeature1Steps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using System.Linq;
+using NUnit.Framework;
+
+namespace DotNetSpain2015.Sam.SpecFlowLab.Intro.Specs
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private Calculator calculator;
+        private List<int> operands;
+        private int result;
+        private int expectedResult = 120;
+
+        public SpecFlowFeature1Steps()
+        {
+            this.calculator = new Calculator();
+            this.operands = new List<int>();
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int operand)
+        {
+            this.operands.Add(operand);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            this.result = this.calculator.Add(this.operands.First(), this.operands.Last());
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(this.expectedResult, this.result);
+        }
+    }
+}
---
+++ DotNetSpain2015.Sam.SpecFlowLab.Intro/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetSpain2015.Sam.SpecFlowLab.Intro
+{
+    public class Calculator
+    {
+        public int Add(int operand1, int operand2)
+        {
+            return operand1 + operand2;
+        }
+    }
+}
