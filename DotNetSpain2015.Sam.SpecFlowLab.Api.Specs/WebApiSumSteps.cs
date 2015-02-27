4386c21af8f32690e2e5c3ac8b97831cb3ca833b
Sam Leach <leachsam@gmail.com>
2015-02-27 12:45:38 +0100

    Created Web Api and Spec template

---
+++ DotNetSpain2015.Sam.SpecFlowLab.Api.Specs/WebApiSumSteps.cs
@@ -0,0 +1,45 @@
+﻿using DotNetSpain2015.Sam.SpecFlowLab.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using System.Linq;
+using System.Web.Http;
+using NUnit.Framework;
+using System.Web.Http.Results;
+
+namespace DotNetSpain2015.Sam.SpecFlowLab.Api.Specs
+{
+    [Binding]
+    public class WebApiSumSteps
+    {
+        private CalculatorController calculatorController;
+        private List<int> operands;
+        private IHttpActionResult httpActionResult;
+
+        public WebApiSumSteps()
+        {
+            this.calculatorController = new CalculatorController();
+            this.operands = new List<int>();
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GinIHaveEnteredIntoTheCalculator(int operand)
+        {
+            this.operands.Add(operand);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            this.httpActionResult = this.calculatorController.Post(this.operands.First(), this.operands.Last());
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            var contentResult = this.httpActionResult as OkNegotiatedContentResult<int>;
+            var sumResult = contentResult.Content;
+            Assert.AreEqual(p0, sumResult);
+        }
+    }
+}
---
+++ DotNetSpain2015.Sam.SpecFlowLab.Api/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using DotNetSpain2015.Sam.SpecFlowLab.Intro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DotNetSpain2015.Sam.SpecFlowLab.Api.Controllers
+{
+    public class CalculatorController : ApiController
+    {
+        private Calculator calculator;
+
+        public CalculatorController()
+        {
+            this.calculator = new Calculator();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return this.Ok();
+        }
+
+        [HttpPost]
+        [Route("calculator/{operand1}/{operand2}")]
+        public IHttpActionResult Post(int operand1, int operand2)
+        {
+            return this.Ok(this.calculator.Add(operand1, operand2));
+        }
+
+        [HttpPost]
+        [Route("calculator/{operand1}/{operand2}")]
+        public IHttpActionResult Subtract(int operand1, int operand2)
+        {
+            // TODO: Use the Subtract calculator method to implement this POST method
+            //return this.Ok(this.calculator.Add(operand1, operand2));
+            return this.InternalServerError();
+        }
+    }
+}
